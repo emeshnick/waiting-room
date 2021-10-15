55f78fa781647f3d11731e83e7c16a7573c79aae
Elijah Meshnick <emeshnick@gmail.com>
2021-10-15 10:53:08 -0400

    chore: set up react-redux

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
-import App from "./app";
+import store from "./store";
 import history from "./history";
+import App from "./app";
 
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
--- client/store/index.js
+++ client/store/index.js
@@ -2,8 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import data from "./data.js";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({ data });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
