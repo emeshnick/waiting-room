fddbf3b030db0ba6695538b3df1f5d90e5759549
Elijah Meshnick <emeshnick@gmail.com>
2021-10-15 11:02:34 -0400

    fix: deleted unecessary history set up

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import store from "./store";
-import history from "./history";
+import history from "history";
 import App from "./app";
 
 ReactDOM.render(
