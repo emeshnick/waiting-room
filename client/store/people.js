50ce31732c0b44d3c33ce51a19ff24a3bb49ecc0
Elijah Meshnick <emeshnick@gmail.com>
2022-01-10 10:05:30 -0700

    refactor add to queue

--- client/store/people.js
+++ client/store/people.js
@@ -1,5 +1,5 @@
 const ADD_PERSON = "ADD_PERSON";
-const GET_PERSON = "GET_PERSON";
+const REMOVE_PERSON = "REMOVE_PERSON";
 
 export const addPerson = (person) => {
   return {
@@ -8,85 +8,87 @@ export const addPerson = (person) => {
   };
 };
 
-function addPriorityQueue(queue, person) {
-  //Redefine queue to not mutate input
-  const newQueue = [...queue, person];
+export const removePerson = () => {
+  return {
+    type: REMOVE_PERSON,
+  };
+};
 
-  let idx = newQueue.length - 1;
+function addPriorityQueue(queue, person) {
+  let idx = queue.length - 1;
   let parentIdx = Math.floor((idx - 1) / 2);
   let temp;
-  while (
-    newQueue[parentIdx] &&
-    person.priority < newQueue[parentIdx].priority
-  ) {
-    temp = newQueue[parentIdx];
-    newQueue[parentIdx] = person;
-    newQueue[idx] = temp;
+  while (queue[parentIdx] && person.priority < queue[parentIdx].priority) {
+    temp = queue[parentIdx];
+    queue[parentIdx] = person;
+    queue[idx] = temp;
 
     idx = parentIdx;
     parentIdx = Math.floor((idx - 1) / 2);
   }
-  return newQueue;
+  return queue;
 }
 
 function removePriorityQueue(queue) {
-  //Redefine queue to not mutate input
-  const newQueue = queue;
+  const queue = queue;
 
   let idx = 0;
   let leftChild = 1;
   let rightChild = 2;
-  let temp = newQueue[newQueue.length - 1];
-  newQueue[newQueue.length - 1] = newQueue[idx];
-  newQueue[idx] = temp;
+  let temp = queue[queue.length - 1];
+  queue[queue.length - 1] = queue[idx];
+  queue[idx] = temp;
 
   //Bubble down the swapped person by priority, while also ordering by time if two children have the same priority
   while (
-    (newQueue[leftChild] && newQueue[idx].priority > newQueue[leftChild]) ||
-    (newQueue[rightChild] && newQueue[idx].priority > newQueue[rightChild])
+    (queue[leftChild] && queue[idx].priority > queue[leftChild]) ||
+    (queue[rightChild] && queue[idx].priority > queue[rightChild])
   ) {
-    if (newQueue[rightChild]) {
-      if (newQueue[leftChild].priority < newQueue[rightChild].priority) {
-        temp = newQueue[leftChild];
-        newQueue[leftChild] = newQueue[idx];
-        newQueue[idx] = temp;
+    if (queue[rightChild]) {
+      if (queue[leftChild].priority < queue[rightChild].priority) {
+        temp = queue[leftChild];
+        queue[leftChild] = queue[idx];
+        queue[idx] = temp;
         idx = leftChild;
-      } else if (newQueue[rightChild].priority < newQueue[leftChild].priority) {
-        temp = newQueue[rightChild];
-        newQueue[rightChild] = newQueue[idx];
-        newQueue[idx] = temp;
+      } else if (queue[rightChild].priority < queue[leftChild].priority) {
+        temp = queue[rightChild];
+        queue[rightChild] = queue[idx];
+        queue[idx] = temp;
         idx = rightChild;
       } else {
-        if (newQueue[leftChild].time < newQueue[rightChild].time) {
-          temp = newQueue[leftChild];
-          newQueue[leftChild] = newQueue[idx];
-          newQueue[idx] = temp;
+        if (queue[leftChild].time < queue[rightChild].time) {
+          temp = queue[leftChild];
+          queue[leftChild] = queue[idx];
+          queue[idx] = temp;
           idx = leftChild;
         } else {
-          temp = newQueue[rightChild];
-          newQueue[rightChild] = newQueue[idx];
-          newQueue[idx] = temp;
+          temp = queue[rightChild];
+          queue[rightChild] = queue[idx];
+          queue[idx] = temp;
           idx = rightChild;
         }
       }
       idx = rightChild;
     } else {
-      temp = newQueue[leftChild];
-      newQueue[leftChild] = newQueue[idx];
-      newQueue[idx] = temp;
+      temp = queue[leftChild];
+      queue[leftChild] = queue[idx];
+      queue[idx] = temp;
       idx = leftChild;
     }
 
     leftChild = idx * 2 + 1;
     rightChild = idx * 2 + 2;
   }
+  queue.pop();
+  return queue;
 }
 
 export default function (state = [], action) {
   switch (action.type) {
     case ADD_PERSON:
-      return addPriorityQueue(state, action.person);
-    case GET_PERSON:
+      const queue = state;
+      return addPriorityQueue(queue, action.person);
+    case REMOVE_PERSON:
       return;
     default:
       return state;
